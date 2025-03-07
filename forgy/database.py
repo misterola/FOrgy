98917f7d06017f9be4a440667712f6ebd951afe1
Lamide Ogundeji <lamide.ogundeji@gmail.com>
2025-03-08 00:57:33 +0100

    Add 'date_created' column to database

--- forgy/database.py
+++ forgy/database.py
@@ -6,15 +6,25 @@ home = Path.home()
 
 # Create 'Books' table in 'library.db' database
 def create_table(destination, table_name):
+    # correct inconsistent naming on columns
     with sqlite3.connect(destination) as connection:
         cursor = connection.cursor()
         cursor.executescript(
             f"""CREATE TABLE {table_name}(
-                Title TEXT, Subtitle TEXT, FullTitle TEXT,
-                Date_of_publication TEXT, Publisher TEXT, Authors TEXT,
-                PageCount TEXT, ISBN10 TEXT, ISBN13 TEXT,
-                RefISBN TEXT, Source TEXT, Filesize REAL
-                );"""
+                    Title TEXT,
+                    Subtitle TEXT,
+                    FullTitle TEXT,
+                    Date_of_publication TEXT,
+                    Publisher TEXT,
+                    Authors TEXT,
+                    PageCount TEXT,
+                    ISBN10 TEXT,
+                    ISBN13 TEXT,
+                    RefISBN TEXT,
+                    Source TEXT,
+                    Filesize REAL,
+                    Date_created TEXT
+            );"""
         )
         print("Book database table created successfully")
 
@@ -32,7 +42,7 @@ def add_metadata_to_table(destination, table_name, values):
         cursor = connection.cursor()
         print("Database connection successful")
         cursor.execute(
-            f"INSERT INTO {table_name} VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+            f"INSERT INTO {table_name} VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?);",
             values,
         )
         print("Book details added successfully")
