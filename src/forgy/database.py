be697edb802a5468ac5e9ebd88e07c67c9bac893
Lamide Ogundeji <lamide.ogundeji@gmail.com>
2025-03-16 22:57:52 +0100

    Clean-up database.py

--- src/forgy/database.py
+++ src/forgy/database.py
@@ -108,37 +108,6 @@ def create_table(destination, table_name="Books", library_name='library.db', del
         print("Database table creation unsuccessfull. Use the create_library_db function to create .db file")
         return None      
 
-### Create 'Books' table in 'library.db' database
-##def create_table(destination, table_name):
-##    # correct inconsistent naming on columns
-##    with sqlite3.connect(destination) as connection:
-##        cursor = connection.cursor()
-##        cursor.executescript(
-##            f"""CREATE TABLE {table_name}(
-##                    Title TEXT,
-##                    Subtitle TEXT,
-##                    FullTitle TEXT,
-##                    Date_of_publication TEXT,
-##                    Publisher TEXT,
-##                    Authors TEXT,
-##                    PageCount TEXT,
-##                    ISBN10 TEXT,
-##                    ISBN13 TEXT,
-##                    RefISBN TEXT,
-##                    Source TEXT,
-##                    Filesize REAL,
-##                    ImageLink TEXT,
-##                    Date_created TEXT
-##            );"""
-##        )
-##        print("Book database table created successfully")
-##
-##
-### Create a 'library.db' database
-##def create_library_db(destination):
-##    with sqlite3.connect(destination) as connection:
-##        cursor = connection.cursor()  # noqa: F841  # no use for cursor variable
-##        print("Database connection established")
 
 # Add metadata to 'Book' table
 def add_metadata_to_table(destination, table_name, values):
