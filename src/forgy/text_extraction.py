43a11f38df32bf38f6b5d821433e58f2dc8df6b0
Lamide Ogundeji <lamide.ogundeji@gmail.com>
2025-04-02 07:12:10 +0100

    Document functions in module. Made some internal functions private.

--- src/forgy/text_extraction.py
+++ src/forgy/text_extraction.py
@@ -1,40 +1,71 @@
-from pypdf import PdfReader
+"""
+A module to extract text from a pdf document.
+The resulting text strin is scanned for ISBN
+with the help of isbn_regex module.
+
+The module also contains a function to directly
+extract in-built book metadata from a pdf document
+"""
+
 from datetime import datetime
+from pathlib import Path
+
+from pypdf import PdfReader
+
 from .metadata_search import get_file_size
 from .isbn_regex import isbn_pattern, format_isbn
+from .logger import create_logger
+
+
+logger = create_logger("text_extraction")
 
 
 def extract_text(pdf_path):
+    """A function to extract text from a PDF ebook
+        as a long string.
+
+    Only the first 20 pages are extracted here as
+    most book ISBNs can be found in this range.
+    """
     extracted_text = ""
 
-    # extract the first n pages of text
+    # Extract the first n pages of text
     try:
         reader = PdfReader(str(pdf_path), strict=False)
-    except ValueError:
+    except ValueError as v:
+        print(f"ValueError encountered: {v}")
         pass
+    except Exception as e:
+        logger.exception(f"An unexpected error occured: {e}")
+        pass
+
     for n in range(1, 20):
         page = reader.pages[n]
         prelim_pages_text = page.extract_text()
         extracted_text = extracted_text + prelim_pages_text
+
     return extracted_text
 
 
 def fetch_metadata_from_file(file):
-    """Function to fetch metadata encoded with pdf book.
+    """Function to fetch in-built metadata encoded with pdf book.
 
-    Not every book has inbuilt metadata, so this will not
-    always yield expected results.
+    Not every book has in-built metadata, so this will not
+    always yield expected results. This unreliability is why
+    it's not been added as a default book
+    metadata
+    source.
     """
-    # Fetch metadata from pdf file
     pdf_reader = PdfReader(file)
     meta = pdf_reader.metadata
     title = meta.title
     subtitle = "NA"
     full_title = meta.title
+
     try:
         date_of_publication = f"{meta.creation_date}"
     except Exception as e:
-        print(f"Error {e} encountered")
+        logger.exception(f"Error {e} encountered")
         date_of_publication = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     publisher = meta.producer
     authors = meta.author
@@ -58,29 +89,45 @@ def fetch_metadata_from_file(file):
         source,
         file_size,
     )
-    # print(file_metadata)
+    logger.info(f"Metadata for {file}: {file_metadata}")
+
     return file_metadata
 
 
-# reader = PdfReader(str(pdf_path), strict=False)
-def extract_last_n_pages(file_path):  # needs reader object
+def _extract_last_n_pages(file_path):
+    """Function to extract text from last 20 pages of a
+    pdf document."""
+
     reader = PdfReader(str(file_path), strict=False)
-    # extract last n pages in a document
     extracted_text = ""
+
     for n in range(1, 20):
         page = reader.pages[-n]
         prelim_pages_text = page.extract_text()
         extracted_text = extracted_text + prelim_pages_text
-        # print(extracted_text)
+
     return extracted_text
 
 
-# extract_last_n_pages(pdf_path)
-def reverse_get_isbn(pdf_path):
-    extracted_text = extract_last_n_pages(pdf_path)
+def _reverse_get_isbn(pdf_path):
+    """Function to extract text from last n pages of a
+    pdf document.
+
+    This is not a very reliable way of extracting ISBN from
+    a book since some publishers do advertise related books
+    at the back of some or all of their books. In this case
+    All ISBNs for different books are matched. This is why
+    getting ISBN from last n pages of a book has not bee applied
+    in FOrgy by default.
+
+    Note: examples of publishers include: Packt, NoStarch Press,
+        and Manning
+    """
+    extracted_text = _extract_last_n_pages(pdf_path)
     matched_isbn = []
     matched_regex = isbn_pattern.findall(extracted_text)
     matched_isbn.append(matched_regex)
     valid_isbn = format_isbn(matched_isbn)
-    print(valid_isbn)
-
+    logger.info(
+        f"Valid ISBNs from {Path(pdf_path).name}: {valid_isbn}"
+    )
