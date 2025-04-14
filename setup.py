81eaab08b2babd451febe88ce2571b3876779b8b
Lamide Ogundeji <lamide.ogundeji@gmail.com>
2025-04-14 21:46:51 +0100

    Add AGPL license to package

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     long_description=read_me,
     long_description_content_type='text/markdown',
     url="https://github.com/misterola/forgy",
+    license="AGPL-3.0",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     install_requires=[
